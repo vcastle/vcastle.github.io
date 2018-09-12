37cb3addef72fee290efd2f3aa9f12e4b4bde67e
Valeria Castillo <valeria.castillo@kraferd.com>
2018-09-12 00:08:12 -0700

    Foundation of Monk's Cafe done, still need to finish reservations table part

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+$('#thumbnails').children('div');
+
+$('#thumbnails').children('div').children('img');
+
+$('#thumbnails').find('.thumbnail');
+  
+$('#thumbnails').children();
+  
+$('.thumbnail').parent().parent('#thumbnails');
+  
+$('.thumbnail').parent('div').next().children('.thumbnail');
+  
+ $('#thumbnails div:last-child').prev().children('.thumbnail');
+});
